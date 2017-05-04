e759ab96552cf2924ddab7cdcdc9ca427cc84ee2
“AlexSun <suncun1995@gmail.com>
2017-05-04 22:06:51 +0800

    on 5.4.2017 ahu,still some bugs

--- dtree.py
+++ dtree.py
@@ -1,16 +1,37 @@
+# -*- coding:utf-8 -*-
 from math import log
+import uniout
+
+
+def cnt_majority(data):
+    cnt = {}
+    for item in data:
+        f = item[-1]
+        if f not in cnt:
+            cnt[f] = 1
+        else:
+            cnt[f] += 1
+    max_times = -1
+    ans = None
+    for key in cnt.keys():
+        if cnt[key] > max_times:
+            max_times = cnt[key]
+            ans = key
+    return
+
+
 def create_decision_tree(data_set, labels):
     # if all the vectors in data_set belong to the same class,return
     class_list = [vector[-1] for vector in data_set]
     class_set = set(class_list)
     if len(class_set) == 1:
-        return
+        return class_list[0]
     # no labels to cut. return
     if len(labels) == 0:
-        return
+        return cnt_majority(data_set)
     label_id = find_best_label(data_set)
-    label = lables[label_id]
-    tree_node = {label:{}}
+    label = labels[label_id]
+    tree_node = {label: {}}
     val_set = set([val for val in data_set[label_id]])
     del(labels[label_id])
     for val in val_set:
@@ -25,17 +46,18 @@ def split_data_set(data_set, label_id, val):
     for vector in data_set:
         if vector[label_id] == val:
             tmp = vector[:label_id]
-            tmp.extend(data_set[label_id + 1 : ])
+            tmp.extend(vector[label_id + 1:])
             new_data_set.append(tmp)
     return new_data_set
 
 
 def find_best_label(data_set):
-    ans = find_by_best_gain(dataset)
+    ans = find_by_best_gain(data_set)
     return ans
 
+
 def find_by_best_gain(data_set):
-    num_features = len(data_set) - 1
+    num_features = len(data_set[0]) - 1
     info_ent = cal_info_ent(data_set)
     best_label = -1
     max_info_gain = -1
@@ -50,11 +72,14 @@ def find_by_best_gain(data_set):
         if info_gain > max_info_gain:
             max_info_gain = info_gain
             best_label = i
+    print 'best label in this round:', best_label
     return best_label
 
 
-def cal_ent(data_set):
-    '''cal the Shannon ent value'''
+def cal_info_ent(data_set):
+    '''
+    cal the Shannon ent value
+    '''
     feature_cnt = {}
     size = len(data_set)
     total_ent = 0.0
@@ -63,21 +88,39 @@ def cal_ent(data_set):
         if cur in feature_cnt.keys():
             feature_cnt[cur] += 1
         else:
-            feature_cnt[cur] = 0
+            feature_cnt[cur] = 1
     for key in feature_cnt.keys():
         prob = float(feature_cnt[key]) / size
-        total_ent -= prob * log(prob,2)
+        print 'prob ', prob
+        total_ent -= prob * log(prob, 2)
     return total_ent
 
+
 def create_data_set(path):
-    ifile = open(path,'r')
-    data_set = []
-    for line in ifile:
-        vector = line.splict(',')
+    i_file = open(path, 'r')
+    data = []
+    label = []
+    first_line = True
+    for line in i_file:
+        vector = line.strip().split(',')
+        if first_line:
+            first_line = False
+            label = vector
+        else:
+            data.append(vector)
+    i_file.close()
+    return data, label
 
-    ifile.close()
+
+if __name__ == '__main__':
+    path = r'C:\Users\User\MLpractice\datasets\wm2.txt'
+    print 'd-tree algorithm start!'
+    print 'read data set where path = ', path
+    data_set, labels = create_data_set(path)
+    # test of create data_set
+    # print data_set
+    # print labels
+    d_tree = create_decision_tree(data_set, labels)
+    print d_tree
 
 
-if __name__ != '__main__':
-    print 'd_tree algorithm'
-    print "test of giiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiit"
